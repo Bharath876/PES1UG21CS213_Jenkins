eb0c3cabc331b28435697fac4dfa0f652d15a91c
Bharath <hmbharath2@gmail.com>
2024-03-10 13:15:47 +0530

    commiting the files

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Changes are made\n" 
+  cout << "Changes are made\n"; 
   cout << "I have successfully built and run\n";
   return 0;
 }
